fc24c79da2edc7d2e58ff4c7ab890b9669aaeeb1
mariosknl <mariosknl@outlook.com>
2021-12-04 07:21:02 +0200

    building middle section of the header

--- components/Header.js
+++ components/Header.js
@@ -1,11 +1,19 @@
 import Image from "next/image";
+import {
+  SearchIcon,
+  PlusCircleIcon,
+  UserGroupIcon,
+  HeartIcon,
+  PaperAirplaneIcon,
+  MenuIcon,
+} from "@heroicons/react/outline";
 
 function Header() {
   return (
     <div>
       <div className="flex justify-between max-w-6xl">
         {/* Left */}
-        <div className="relative w-24 h-24 hidden lg:inline-grid cursor-pointer">
+        <div className="relative w-24 hidden lg:inline-grid cursor-pointer">
           <Image
             src="https://links.papareact.com/ocw"
             layout="fill"
@@ -13,7 +21,7 @@ function Header() {
           />
         </div>
 
-        <div className="relative w-10 h-10 lg:hidden flex-shrink-0">
+        <div className="relative w-10 lg:hidden flex-shrink-0">
           <Image
             src="https://links.papareact.com/jjm"
             layout="fill"
@@ -21,7 +29,20 @@ function Header() {
           />
         </div>
 
-        {/* Middle */}
+        {/* Middle - Search input field */}
+        <div className="max-w-xs">
+          <div className="mt-1 relative p-3 rounded-md">
+            <div className="absolute inset-y-0 pl-3 flex items-center pointer-events-none">
+              <SearchIcon className="h-5 w-5 text-gray-500" />
+            </div>
+            <input
+              type="text"
+              placeholder="Search"
+              className="bg-gray-50 block w-full pl-10 sm:text-sm border-gray-300
+            rounded-md focus:ring-black focus:border-black"
+            />
+          </div>
+        </div>
 
         {/* Right */}
       </div>
